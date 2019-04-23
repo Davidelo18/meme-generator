366725513e49dc9dd2001f1364b7dc7cc2f64b65
davidelo <daw.kielbik@gmail.com>
2019-04-23 16:25:36 +0200

    creating basic image

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const memeTemplate = document.querySelectorAll('.meme-template');
+const radioTemplate = document.querySelectorAll('.radio-template');
 const topText = document.getElementById('topText');
 const bottomText = document.getElementById('bottomText');
 const generateBtn = document.getElementById('genereteBtn');
@@ -6,11 +7,20 @@ const memeForm = document.getElementById('memeForm');
 const memeCanvas = document.getElementById('memeArea');
 const ctx = memeCanvas.getContext('2d');
 
+memeCanvas.height = memeCanvas.width = 0;
+
 memeForm.addEventListener('submit', (e) => {
     if (topText.value == '' || bottomText.value == '') {
         e.preventDefault();
         alert('Write some text to meme');
+        return false;
     }
+
+    let template = new Image();
+    radioTemplate.forEach((radio, index, theArray) => {if (theArray[index].checked == true) template.src = memeTemplate[index].src});
+
+    generateMeme(template);
+    console.log(template);
 });
 
 function generateMeme(template) {
