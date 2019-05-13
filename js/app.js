e705fa9c32ce21be12f88c6758733ba6591174cc
davidelo <daw.kielbik@gmail.com>
2019-05-13 23:26:16 +0200

    create text feature

--- js/app.js
+++ js/app.js
@@ -14,18 +14,35 @@ memeForm.addEventListener('submit', (e) => {
         e.preventDefault();
         alert('Write some text to meme');
         return false;
+    } else if () {
+        e.preventDefault();
+        alert('Choose one template to meme');
+        return false;
     }
 
     let template = new Image();
     radioTemplate.forEach((radio, index, theArray) => {if (theArray[index].checked == true) template.src = memeTemplate[index].src});
 
-    generateMeme(template);
-    console.log(template);
+    generateMeme(template, topText.value, bottomText.value);
 });
 
-function generateMeme(template) {
+function generateMeme(template, top, bottom) {
     memeCanvas.width = template.width;
     memeCanvas.height = template.height;
     ctx.clearRect(0, 0, memeCanvas.width, memeCanvas.height);
     ctx.drawImage(template, 0, 0);
+
+    const fontSize = memeCanvas.width / 15;
+    ctx.font = `${fontSize}px Impact`; 
+    ctx.fillStyle = '#fff';
+    ctx.strokeStyle = '#000';
+    ctx.textAlign = 'center';
+
+    ctx.textBaseline = 'top';
+    ctx.fillText(top.toUpperCase(), memeCanvas.width / 2, 5, memeCanvas.width);
+    ctx.strokeText(top.toUpperCase(), memeCanvas.width / 2, 5, memeCanvas.width);
+
+    ctx.textBaseline = 'bottom';
+    ctx.fillText(bottom.toUpperCase(), memeCanvas.width / 2, memeCanvas.height, memeCanvas.width);
+    ctx.strokeText(bottom.toUpperCase(), memeCanvas.width / 2, memeCanvas.height, memeCanvas.width);
 }
